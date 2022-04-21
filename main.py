a0d408905093305ca198d51ba8273b31dca815af
Gabriel <gbialystokjoly@gmail.com>
2022-04-21 11:35:33 -0400

    Fixed some bugs. Such as balance not working within ifs.
    Co-authored-by: hogefoot <hogefoot@users.noreply.github.com>

--- main.py
+++ main.py
@@ -5,37 +5,41 @@ def getBal(use):
     if use == 1:#log
         balance = balance
         print(f"Your current balance is {balance}, you have made {balance - 100000}$ so far. You need {1000000000 - balance} more dollars to win.")
+        return ""
     elif use == 2:#reset
         balance = 100000
     elif use == 3:#return
         return balance
 def getgamePlay():
-   gametoplay = input(f"What game would you like to play? {getBal(use)} ").lower()
+   gametoplay = input(f"What game would you like to play (roulette, slots)? {getBal(1)} ").lower()
    return gametoplay
 
 gametoplay = getgamePlay()
 
 
 if gametoplay == ("roulette"):
-     print("You walk up to a free roulette table. \nA worker tells you to take a seat and to place your bets \n")
-     betforroulette = int(input(f"What is your bet, your current balance is: {balance}: "))
-     print(f"You bet {betforroulette}.\n")
-     rouletteplacenum = int(input("What number do you want to place your bets on? (1 - 31)\n"))
-     if rouletteplacenum > 31:
+    balance =  getBal(3)
+    print("You walk up to a free roulette table. \nA worker tells you to take a seat and to place your bets \n")
+    betforroulette = int(input(f"What is your bet, your current balance is: {balance}: "))
+    if betforroulette >> getBal(3):
+        print(f"Thats to much.\nYou need {betforroulette - balance} more")
+    print(f"You bet {betforroulette}.\n")
+    rouletteplacenum = int(input("What number do you want to place your bets on? (1 - 31)\n"))
+    if rouletteplacenum > 31:
          print("That's not a valid number. The roulette worker tells you to leave if you're going to be trolling. You get kicked out.")
          exit()
-     else:
-         print(f"You place your {betforroulette}$ bet on number {rouletteplacenum}")
-         roulettenumland = random.randint(1, 31)
-         print("The worker spins the wheel...\n")
-         if rouletteplacenum == roulettenumland:
-             print(f"It lands on {roulettenumland} and you win! Since you bet {betforroulette} your bet gets doubled, meaning you won {betforroulette * 2}$ Your balance is now {balance + betforroulette * 2}")
-             balance = int((betforroulette * 2))
-             getgamePlay()
-         else:
-             print(f"It lands on {roulettenumland} and you loose! Since you bet {betforroulette} your bet gets taken by the house, meaning you lost {betforroulette}$ Your balance is now {balance - betforroulette}")
-             balance = int((balance - betforroulette))
-             getgamePlay()
+    else:
+        print(f"You place your {betforroulette}$ bet on number {rouletteplacenum}")
+        roulettenumland = random.randint(1, 31)
+        print("The worker spins the wheel...\n")
+        if rouletteplacenum == roulettenumland:
+            print(f"It lands on {roulettenumland} and you win! Since you bet {betforroulette} your bet gets doubled, meaning you won {betforroulette * 2}$ Your balance is now {balance + betforroulette * 2}")
+            balance = int((betforroulette * 2))
+            getgamePlay()
+        else:
+            print(f"It lands on {roulettenumland} and you loose! Since you bet {betforroulette} your bet gets taken by the house, meaning you lost {betforroulette}$ Your balance is now {balance - betforroulette}")
+            balance = int((balance - betforroulette))
+            getgamePlay()
 if gametoplay == ("slots"):
     print("You walk up to the slots machine. \nYou see the machine and it askes you what you want to bet")
     betforslots = int(input(f"What is your bet, your current balance is: {balance}: "))
